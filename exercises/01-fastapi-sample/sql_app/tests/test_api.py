0679ff5775fa3616e47b47e2eeb1a3e78ae1967d
kojipy <43063676+kojipy@users.noreply.github.com>
2022-08-14 00:14:28 +0900

    update:認証まわりのテストコードの追加

--- exercises/01-fastapi-sample/sql_app/tests/test_api.py
+++ exercises/01-fastapi-sample/sql_app/tests/test_api.py
@@ -16,3 +16,41 @@ def test_create_user(test_db, client):
     data = response.json()
     assert data["email"] == "deadpool@example.com"
     assert data["id"] == user_id
+
+
+def test_authorize(test_db, client):
+    response = client.post(
+        "/users/",
+        json={"email": "sameone@example.com", "password": "passpasspass"},
+    )
+    assert response.status_code == 200, response.text
+
+    token = response.headers["x-api-token"]
+    data = response.json()
+    user_id = data["id"]
+
+    # correct token
+    response = client.get(f"/users/{user_id}", headers={"x-api-token": token})
+    assert response.status_code == 200, response.text
+
+    # incorrect token
+    response = client.get(f"/users/{user_id}", headers={"x-api-token": "abc"})
+    assert response.status_code == 401, response.text
+
+
+def test_inclued_api_token_request(test_db, client):
+    response = client.post(
+        "/users/",
+        json={"email": "dummy@example.com", "password": "fakepassword"},
+    )
+    assert response.status_code == 200, response.text
+    token = response.headers["x-api-token"]
+    data = response.json()
+    user_id = data["id"]
+
+    response = client.get(f"/users/{user_id}", headers={"x-api-token": token})
+    assert response.status_code == 200, response.text
+
+    # incorrect token
+    response = client.get(f"/users/{user_id}")
+    assert response.status_code == 401, response.text
