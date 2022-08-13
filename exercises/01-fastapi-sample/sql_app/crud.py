93f9aca2372b68cdcb555ff7da8be08dea227623
kojipy <43063676+kojipy@users.noreply.github.com>
2022-08-13 23:37:12 +0900

    update:トークンが有効か確認する処理を追加

--- exercises/01-fastapi-sample/sql_app/crud.py
+++ exercises/01-fastapi-sample/sql_app/crud.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy.orm import Session
 
 from . import auth, models, schemas
@@ -38,3 +40,9 @@ def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db.commit()
     db.refresh(db_item)
     return db_item
+
+
+def authorized_tokens(db: Session):
+    return (
+        db.query(models.Token).filter(models.Token.token_limit > datetime.now()).all()
+    )
--- exercises/01-fastapi-sample/sql_app/main.py
+++ exercises/01-fastapi-sample/sql_app/main.py
@@ -30,6 +30,13 @@ db_session = Depends(get_db)
 has_token = Depends(has_api_token)
 
 
+def check_authorized(db: Session, token: str):
+    authorized_tokens = crud.authorized_tokens(db)
+    authorized_tokens_str: List[str] = [t.token for t in authorized_tokens]
+    if token not in authorized_tokens_str:
+        raise HTTPException(status_code=401, detail="token has expired")
+
+
 @app.get("/health-check")
 def health_check(db: Session = db_session):
     return {"status": "ok"}
@@ -53,6 +60,7 @@ def read_users(
     request: Request = has_token,
     db: Session = db_session,
 ):
+    check_authorized(db, request.headers["x-api-token"])
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
@@ -63,6 +71,7 @@ def read_user(
     request: Request = has_token,
     db: Session = db_session,
 ):
+    check_authorized(db, request.headers["x-api-token"])
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -76,6 +85,7 @@ def create_item_for_user(
     request: Request = has_token,
     db: Session = db_session,
 ):
+    check_authorized(db, request.headers["x-api-token"])
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
@@ -86,5 +96,6 @@ def read_items(
     request: Request = has_token,
     db: Session = db_session,
 ):
+    check_authorized(db, request.headers["x-api-token"])
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
