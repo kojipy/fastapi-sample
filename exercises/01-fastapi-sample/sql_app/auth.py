1a08242a5a02d0ce824c44c052ee944fa31703b8
kojipy <43063676+kojipy@users.noreply.github.com>
2022-08-14 14:33:23 +0900

    refactor:パスワードダミーハッシュ関数を移動

--- exercises/01-fastapi-sample/sql_app/auth.py
+++ exercises/01-fastapi-sample/sql_app/auth.py
@@ -2,11 +2,15 @@ import hashlib
 from datetime import datetime, timedelta
 from typing import Tuple
 
+TOKEN_LIMIT = timedelta(days=7)
 
-def create_token(
-    delta: timedelta = timedelta(days=0, minutes=10)
-) -> Tuple[str, datetime]:
+
+def create_token(delta: timedelta = TOKEN_LIMIT) -> Tuple[str, datetime]:
     now = datetime.now()
-    token = hashlib.md5(str(now).encode("utf-8")).hexdigest()
     limit = now + delta
+    token = hashlib.md5(str(now).encode("utf-8")).hexdigest()
     return token, limit
+
+
+def get_hashed_password(password: str):
+    return password + "notreallyhashed"
